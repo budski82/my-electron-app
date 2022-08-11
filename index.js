6af8e436b31cdcc019d6359b5f06c433c70577c2
William Rutkowski <budski82@users.noreply.github.com>
2022-08-11 14:44:41 -0400

    modify js index

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
-let myOBJ = {
-	msg0: 'Electron is shocking',
-	msg1: 'Electron is nothing without Tesla',
-	msg2: 'Electron is Green!',
-};
+const { app, BrowserWindow } = require('electron')
 
-console.log(`Hello from ${myOBJ.msg1}`);
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+  createWindow()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myOBJ = {
+// 	msg0: 'Electron is shocking',
+// 	msg1: 'Electron is nothing without Tesla',
+// 	msg2: 'Electron is Green!',
+// };
+
+// console.log(`Hello from ${myOBJ.msg1}`);
