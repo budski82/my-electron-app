bf9d10ccf656e65fad3b03cc16ff5555c0d92dd5
William Rutkowski <budski82@users.noreply.github.com>
2022-08-12 04:11:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const createWindow = () => {
     height: 600,
   })
   win.loadFile('index.html')
-}
+};
+
 app.whenReady().then(() => {
   createWindow()
 })
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
+
+app.on('window-all-closed', () => {
+if (process.platform !== 'darwin') {
+	app.quit();
+}
+});
 
 
 
